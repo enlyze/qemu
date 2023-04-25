b96a63d5472d821327f5f02ac492c752ca4d129a
Tom Dohrmann <t.dohrmann@enlyze.com>
2023-04-25 11:05:32 +0200

    make bios memory region writable
    The firmware expects the ACPI RSDP to be in this region, however this
    won't be the case if we launch qemu with UEFI. Instead we write the RSDP
    to that location ourselves in the bootloader.

--- hw/i386/pc_sysfw.c
+++ hw/i386/pc_sysfw.c
@@ -67,8 +67,6 @@ static void pc_isa_bios_init(MemoryRegion *rom_memory,
     memcpy(isa_bios_ptr,
            ((uint8_t*)flash_ptr) + (flash_size - isa_bios_size),
            isa_bios_size);
-
-    memory_region_set_readonly(isa_bios, true);
 }
 
 static PFlashCFI01 *pc_pflash_create(PCMachineState *pcms,
